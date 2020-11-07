4f8c99a0b0d943dc87cc761617bce38e76e43b8e
[Yufei <[yzhu0685@uni.sydney.edu.au]>
2020-11-08 04:04:39 +1100

    Completed Post page with footer and css file. Add comments for CommentService, Article Service, and Post controller.

--- ELEC5619_HobbyNexus/src/main/java/au/usyd/nexus/service/ArticleService.java
+++ ELEC5619_HobbyNexus/src/main/java/au/usyd/nexus/service/ArticleService.java
@@ -2,6 +2,7 @@ package au.usyd.nexus.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import au.usyd.nexus.dao.ArticleDAO;
 import au.usyd.nexus.domain.Article;
@@ -13,6 +14,14 @@ public class ArticleService {
 	@Autowired
 	private ArticleDAO articleDAO;
 	
+    /**
+     * This function returns Article object with the article id
+     * 
+     * @param id 
+     * 
+     * @return : null if id does not exist
+     * 		 : Article object if id exists
+     */
 	public Article getArticleById(Integer id) {
 		return articleDAO.getArticleById(id);
 	}
--- ELEC5619_HobbyNexus/src/main/java/au/usyd/nexus/service/CommentService.java
+++ ELEC5619_HobbyNexus/src/main/java/au/usyd/nexus/service/CommentService.java
@@ -14,18 +14,50 @@ public class CommentService {
 	@Autowired
 	private CommentDAO commentDAO;
 	
+    /**
+     * This function returns Comment object with the comment id
+     * 
+     * @param id 
+     * 
+     * @return : null if id does not exist
+     * 		 : Comment object if id exists
+     */
     public Comment getCommentById(Integer id) {
     	return commentDAO.getCommentById(id);
     }
     
+    
+    /**
+     * This function inserts new Comment object to database
+     * 
+     * @param comment: New Comment object
+     * 
+     */
     public void addComment(Comment comment) {
     	commentDAO.addComment(comment);
     }
     
+    
+    /**
+     * This function returns Comment objects for specific article with article id
+     * 
+     * @param id 
+     * 
+     * @return : null if id does not exist
+     * 		 : Comment objects if id exists
+     */
     public List<Comment> getCommentsByArticleId(Integer id){
     	return commentDAO.getCommentsByArticleId(id);
     }
     
+    /**
+     * This function returns user object with given user id
+     * 
+     * @param id 
+     * 
+     * @return : null if id does not exist
+     * 		 : user object if id exists
+     */
     public User getUserById(Integer id) {
     	return commentDAO.getUserById(id);
     }
